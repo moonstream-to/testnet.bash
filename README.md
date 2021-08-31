d9b8adf2c23d22b5499a11d1e61910744fe68b2f
Neeraj Kashyap <nkashy1@gmail.com>
2021-08-31 13:43:29 -0700

    Added testnet.bash and README

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # testnet.bash
 Bash script that sets up a private Ethereum network for development and testing.
+
+## Setup
+
+This bash script uses:
+0. [`bash`](https://www.gnu.org/software/bash/)
+1. [`geth`](https://github.com/ethereum/go-ethereum)
+2. [`jq`](https://github.com/stedolan/jq)
+
+
+## Creating a test network
+
+To bring up a test network, run the [`testnet.bash`](./testnet.bash) script from the root of this
+repository as follows:
+```bash
+mkdir -p testnet
+TESTNET_BASE_DIR=testnet/ ./testnet.bash
+```
+
+You can run the same command to restart the testnet after shutting it down using `CTRL+C`.
+
+All the account passwords (by default) are `peppercat`.
+
+## Inputs
+
+Accepts inputs through environment variables:
+- `TESTNET_BASE_DIR`: Directory in which all data is stored. This is where you will find your node
+data directories, dag directories, and more.
+
+## Customization
+
+Just copy the bash script and edit it, my friend.
+
+## FAQ
+
+### Why not [Ganache](https://www.trufflesuite.com/ganache)?
+
+Ganache is pretty heavy-duty and I wanted something light-weight that I could use to spin up a private
+network, set up a scenario, and then run a series of tests.
+
+Ganache is also a completely independent implementation of the Ethereum interfaces. I wanted to be
+able to test against (different versions of) `geth` because that's what we currently use in
+[Moonstream](https://github.com/bugout-dev/moonstream). Moonstream crawlers crawl data from `geth`
+nodes and any reasonable test of those crawlers should use `geth` nodes.
+
+### Why bash?
+
+Because `bash` is ubiquitous, especially in continuous integration environments.
+
+I have experimented in the past with similar setups in `docker-compose` ([link](https://github.com/the-chaingang/ethereal)).
+This bash script does the job just as well, is much simpler to understand, and is much easier to
+customize.
+
+### How do I change the genesis parameters?
+
+Copy the bash script and edit the definition of the `GENESIS_JSON` variable.
+
+### How do I add more nodes?
+
+The vanilla script sets up 2 mining nodes. Any less and chain data will not be persisted to disk
+between runs. If you wanted to add a third node, you would modify the bash script to add a `run_miner`
+call:
+```bash
+MINER_0=$(run_miner 0)
+MINER_1=$(run_miner 1)
+MINER_2=$(run_miner 2)
+```
+
+If you wanted to print metadata about this new miner, you would add a new `echo` statement near
+where the miner metadata is printed:
+```bash
+echo "Running testnet. Miner info:"
+echo "$MINER_0" | jq .
+echo "$MINER_1" | jq .
+echo "$MINER_2" | jq .
+echo
+echo "Press CTRL+C to exit."
+```
+
+Finally, you would probably want to add the new miner's logs to the tail command:
+```bash
+tail -f $(echo "$MINER_0" | jq -r ".logfile") $(echo "$MINER_1" | jq -r ".logfile")
+```
+
+### Why `geth`?
+
+If you would like to add support for another Ethereum client, I would welcome a pull request.
+
+### Why does this script fail when I try to use a temporary testnet base directory on my Mac?
+
+This script uses `mktemp -d` when you don't specify a testnet directory.
+
+The BSD version of `mktemp` has a different signature than the GNU version. Relevant StackExchange
+thread: https://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
+
+If you want to run this script on a Mac with a temporary base directory, please let me know and I
+will add support. At that time, I would appreciate your help in testing the change.
+
+In the meantime, you can get around the issue by explicitly creating a base directory and setting it
+as the `TESTNET_BASE_DIR` environment variable.
+
+### Where have you tested `testnet.bash`?
+
+So far, it has been tested on:
+- Debian Buster VM image on WSL2 (Windows 10), bash `5.0.3(1)-release`
+
+If you have run this script successfully, I would appreciate it if you submit an edit to this README
+describing your environment. Thank you.
+
+### `peppercat`?
+
+<img src="https://s3.amazonaws.com/static.simiotics.com/pepper/pepper-tennis-ball.jpg" width="400"/>
+
+## Contributing
+
+Your contributions are welcome:
+- [Create an issue](https://github.com/bugout-dev/testnet.bash/issues/new)
+- [Open a pull request](https://github.com/bugout-dev/testnet.bash/compare)
---
+++ testnet.bash
@@ -0,0 +1,180 @@
+#!/usr/bin/env bash
+
+# This script sets up an Ethereum test network using 2 miners.
+# Assumptions:
+# - geth available on PATH
+# - jq available on PATH
+# - The password is always "peppercat" (without the quotes)
+
+# Accepts the following environment variables as inputs:
+# - TESTNET_BASE_DIR: Directory into which all testnet data goes for all nodes.
+
+set -e -o pipefail
+
+PASSWORD_FOR_ALL_ACCOUNTS="peppercat"
+
+TESTNET_BASE_DIR="$TESTNET_BASE_DIR"
+if [ -z "$TESTNET_BASE_DIR" ]
+then
+    TESTNET_BASE_DIR="$(mktemp -d)"
+    echo "TESTNET_BASE_DIR not provided. Using temporary directory: $TESTNET_BASE_DIR" 1>&2
+fi
+
+if [ ! -d "$TESTNET_BASE_DIR" ]
+then
+    echo "Base directory does not exist or is not a directory: $TESTNET_BASE_DIR"
+    exit 1
+fi
+
+PIDS_FILE="$TESTNET_BASE_DIR/pids.txt"
+if [ ! -f "$PIDS_FILE" ]
+then
+    touch "$PIDS_FILE"
+fi
+
+BOOTNODES_FILE="$TESTNET_BASE_DIR/bootnodes.txt"
+if [ ! -f "$BOOTNODES_FILE" ]
+then
+    touch "$BOOTNODES_FILE"
+fi
+
+# Reset PID and bootnode metadata
+rm -f "$BOOTNODES_FILE" "$PIDS_FILE"
+
+GENESIS_JSON="$TESTNET_BASE_DIR/genesis.json"
+cat <<EOF >"$GENESIS_JSON"
+{
+  "config": {
+    "chainId": 1337,
+    "homesteadBlock": 0,
+    "eip150Block": 0,
+    "eip155Block": 0,
+    "eip158Block": 0,
+    "byzantiumBlock": 0,
+    "constantinopleBlock": 0,
+    "petersburgBlock": 0,
+    "istanbulBlock": 0,
+    "berlinBlock": 0
+  },
+  "alloc": {},
+  "coinbase": "0x0000000000000000000000000000000000000000",
+  "difficulty": "0x20000",
+  "extraData": "",
+  "gasLimit": "0x2fefd8",
+  "nonce": "0x0037861100000042",
+  "mixhash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+  "timestamp": "0x00"
+}
+EOF
+
+function run_miner() {
+    PASSWORD_FILE="$TESTNET_BASE_DIR/password.txt"
+    if [ ! -f "$PASSWORD_FILE" ]
+    then
+        echo "$PASSWORD_FOR_ALL_ACCOUNTS" >"$PASSWORD_FILE"
+    fi
+    MINER_LABEL="miner-$1"
+    MINER_LOGFILE="$TESTNET_BASE_DIR/$MINER_LABEL.log"
+    MINER_DATADIR="$TESTNET_BASE_DIR/$MINER_LABEL"
+    echo "Creating data directory for miner: $MINER_LABEL -- $MINER_DATADIR" 1>&2
+    mkdir "$MINER_DATADIR"
+
+    ETHASH_DIR="$TESTNET_BASE_DIR/ethash"
+    if [ ! -d "$ETHASH_DIR" ]
+    then
+        mkdir "$ETHASH_DIR"
+    fi
+    MINER_DAGDIR="$ETHASH_DIR/$MINER_LABEL"
+
+    KEYSTORE_DIR="$MINER_DATADIR/keystore"
+    mkdir -p "$KEYSTORE_DIR"
+    OLDEST_ACCOUNT_FILE=$(ls -1tr "$KEYSTORE_DIR")
+    if [ -z "$OLDEST_ACCOUNT_FILE" ]
+    then
+        geth account new --datadir "$MINER_DATADIR" --password "$PASSWORD_FILE" >>"$MINER_LOGFILE"
+        OLDEST_ACCOUNT_FILE=$(ls -1tr "$MINER_DATADIR/keystore/")
+    fi
+
+    MINER_ADDRESS=$(jq -r ".address" "$KEYSTORE_DIR/$OLDEST_ACCOUNT_FILE")
+
+    geth init --datadir "$MINER_DATADIR" "$GENESIS_JSON"
+
+    BOOTNODE="$(head -n1 $BOOTNODES_FILE)"
+
+    if [ -z "$BOOTNODE" ]
+    then
+        set -x
+        geth \
+            --datadir="$MINER_DATADIR" \
+            --ethash.dagdir="$MINER_DAGDIR" \
+            --mine \
+            --miner.threads=1 \
+            --miner.gasprice=1000 \
+            --miner.etherbase="$MINER_ADDRESS" \
+            --networkid=1337 \
+            --port 0 \
+            >>"$MINER_LOGFILE" 2>&1 \
+            &
+        set +x
+    else
+        set -x
+        geth \
+            --datadir="$MINER_DATADIR" \
+            --ethash.dagdir="$MINER_DAGDIR" \
+            --mine \
+            --miner.threads=1 \
+            --miner.gasprice=1000 \
+            --miner.etherbase="$MINER_ADDRESS" \
+            --networkid=1337 \
+            --port 0 \
+            --bootnodes "$BOOTNODE" \
+            >>"$MINER_LOGFILE" 2>&1 \
+            &
+        set +x
+    fi
+
+    PID="$!"
+    echo "$PID" >>"$PIDS_FILE"
+
+    if [ -z "$BOOTNODE" ]
+    then
+        until geth attach --exec "console.log(admin.nodeInfo.enode)" "$MINER_DATADIR/geth.ipc" | head -n1 >"$BOOTNODES_FILE"
+        do
+            sleep 1
+        done
+    fi
+
+    echo "{\"miner\": \"$MINER_LABEL\", \"address\": \"$MINER_ADDRESS\", \"pid\": $PID, \"logfile\": \"$MINER_LOGFILE\"}"
+}
+
+function cancel() {
+    while read -r pid
+    do
+        echo "Killing process: $pid" 1>&2
+        kill -2 "$pid"
+    done <"$PIDS_FILE"
+
+    while read -r pid
+    do
+        while kill -0 "$pid"
+        do
+            echo "Waiting for process to die..." 1>&2
+            sleep 1
+        done
+        echo "Process killed: $pid" 1>&2
+    done <"$PIDS_FILE"
+}
+
+trap cancel SIGINT
+
+MINER_0=$(run_miner 0)
+MINER_1=$(run_miner 1)
+
+echo "Running testnet. Miner info:"
+echo "$MINER_0" | jq .
+echo "$MINER_1" | jq .
+echo
+echo "Press CTRL+C to exit."
+
+tail -f $(echo "$MINER_0" | jq -r ".logfile") $(echo "$MINER_1" | jq -r ".logfile")
